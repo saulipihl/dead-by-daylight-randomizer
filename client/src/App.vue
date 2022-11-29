bfae47b5d510fb0c3aa551b0e9effec2c467d506
Sauli Pihl <sauli.pihl@gmail.com>
2022-11-29 02:13:35 +0200

    Initial commit with the header component

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class>
+    <header>
+      <AppHeader/>
+    </header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/Header.vue';
+document.title = 'DBD Randomizer';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+<style>
+#app, button {
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
+  color: white;
+}
+html {
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('~@/assets/images/background.png');
+  background-size: cover;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+body {
+  margin: 0px;
+}
+</style>
---
+++ client/src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header-container">
+    <div class="logo-section">
+      <img id="logo" src="@/assets/images/dbd-icon.svg"/>
+      <h2>DEAD BY DAYLIGHT RANDOMIZER</h2>
+    </div>
+    <div class="actions">
+      <button>Survivor</button>
+      <button>Killer</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header-container {
+    height: 100px;
+    display: grid;
+    grid-template-columns: auto auto;
+    justify-items: center;
+    align-items: center;
+  }
+
+  h2 {
+    margin: 0;
+    align-self: center;
+  }
+
+  .logo-section {
+    height: inherit;
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    justify-items: center;
+    gap: 1rem;
+  }
+
+  .actions {
+    display: flex;
+    flex-direction: row;
+    text-align: center;
+    justify-items: center;
+    gap: 2rem;
+  }
+
+  .actions button {
+    border: none;
+    background: none;
+    font-size: 1rem;
+    cursor: pointer;
+  }
+
+  .actions button:hover {
+    color: red;
+    transition: color 0.2s;
+  }
+</style>
