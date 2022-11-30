dc31d99647ef2934dfeb9316371e7b3827d6ac73
Sauli Pihl <sauli.pihl@gmail.com>
2022-11-30 02:51:34 +0200

    Add tooltip to the perks

--- client/src/components/Perk.vue
+++ client/src/components/Perk.vue
@@ -1,6 +1,8 @@
 <template>
     <div :style="{ 'backgroundImage': 'url(\'' + image() + '\')'}" class="perk-container">
-
+        <div class="tooltip-container">
+            <span class="tooltiptext">{{ perk.name }}</span>
+        </div>
     </div>
 </template>
 
@@ -13,7 +15,7 @@
         methods: {
             image() {
                 return require(`../assets/images/${this.perk.target}-perks/${this.perk.imageName}.webp`);
-            }
+            },
         }
     }
 </script>
@@ -24,6 +26,28 @@
         height: 120px;
         position: absolute;
         background-size: cover;
-        background-color: blueviolet;
+        background-color: #3a185a;
+    }
+    .tooltip-container {
+        position: relative;
+    }
+    .tooltip-container .tooltiptext {
+        visibility: hidden;
+        width: 120px;
+        background-color: black;
+        color: #fff;
+        text-align: center;
+        border-radius: 6px;
+        border: 3px solid red;
+        padding: 5px 0;
+
+        position: absolute;
+        z-index: 1;
+        rotate: -45deg;
+        transform: translateX(90px) translateY(-20px);
+    }
+
+    .perk-container:hover .tooltiptext {
+        visibility: visible;
     }
 </style>
