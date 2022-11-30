2cbcce89bf324c1e30278d2f7351d4c16c086367
Sauli Pihl <sauli.pihl@gmail.com>
2022-12-01 00:03:56 +0200

    Clean the project structure

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "https://saulipihl.github.io/dead-by-daylight-randomizer/" : "",
+  publicPath: process.env.NODE_ENV === "production" ? "/dead-by-daylight-randomizer/" : "/",
 })
